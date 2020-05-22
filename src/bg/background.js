9595fc759bea1663112dfbd999f5593b9f55e125
Emil Shakirov <me+github@emil.sh>
2020-05-22 17:29:42 +0200

    Handle accessTokenRequest in background script

--- src/bg/background.js
+++ src/bg/background.js
@@ -5,5 +5,20 @@ chrome.extension.onMessage.addListener(function (
 ) {
   chrome.pageAction.show(sender.tab.id);
 
-  sendResponse();
+  switch (request.messageType) {
+    case "accessTokenRequest":
+      chrome.identity.getAuthToken({ interactive: false }, function (token) {
+        chrome.storage.local.set({ ["loggedIn"]: true }, function () {
+          console.log("User has logged in.");
+        });
+        sendResponse({ accessToken: token });
+      });
+
+      break;
+    default:
+      sendResponse();
+      break;
+  }
+
+  return true;
 });
